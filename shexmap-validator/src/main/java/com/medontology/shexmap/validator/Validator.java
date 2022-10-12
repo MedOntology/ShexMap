83ab40cfd882ab642082cf07b2010e9b7acfddd5
cnanjo@gmail.com <cnanjo@gmail.com>
2022-10-12 00:53:43 -0700

    Cleaned up validator to pass shape and node arguments

--- shexmap-validator/src/main/java/com/medontology/shexmap/validator/Validator.java
+++ shexmap-validator/src/main/java/com/medontology/shexmap/validator/Validator.java
@@ -1,19 +1,30 @@
 package com.medontology.shexmap.validator;
 
 import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.Node;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
 import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.shex.Shex;
-import org.apache.jena.shex.ShexSchema;
-import org.apache.jena.shex.ShexValidator;
+import org.apache.jena.shex.*;
 
 public class Validator {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 
-    public void validateSchema(String schemaPath, String baseUri, String dataPath) {
-        ShexSchema schema = Shex.readSchema(schemaPath, baseUri);
+    public boolean validateSchema(String schemaPath, String schemaBase, String dataPath, String nodeStr, String shapeStr) {
+        Model model = ModelFactory.createDefaultModel();
+        Node node = model.createResource(nodeStr).asNode();
+        Node shapeRef = model.createResource(shapeStr).asNode();
+        ShexMapBuilder b = new ShexMapBuilder();
+        b.add(node, shapeRef);
+
         Graph graph = RDFDataMgr.loadGraph(dataPath);
-        ShexValidator.get().validate(graph, schema, schema.getShapes().get(0), graph.find().next().getSubject());
+        //RDFDataMgr.read(Model model, String uri, String base, Lang hintLang)
+        ShexSchema schema = Shex.readSchema(schemaPath, schemaBase);
+        ShexReport report = ShexValidator.get().validate(graph, schema, b.build());
+
+        return report.conforms();
     }
 }
--- shexmap-validator/src/test/java/com/medontology/shexmap/validator/ValidatorTest.java
+++ shexmap-validator/src/test/java/com/medontology/shexmap/validator/ValidatorTest.java
@@ -1,19 +1,20 @@
 package com.medontology.shexmap.validator;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class ValidatorTest {
 
     @Test
     public void test() {
         Validator validator = new Validator();
-        validator.validateSchema("1iri.shex", "http://a.example", "Is1_Ip1_Io1.ttl");
+        assertTrue(validator.validateSchema("1iri.shex", "http://a.example", "Is1_Ip1_Io1.ttl", "http://a.example/s1", "http://a.example/S1"));
     }
 
     @Test
     public void test2() {
         Validator validator = new Validator();
-        validator.validateSchema("shexmap_example1.shex", "http://dam.example/med", "shexmap_data_example1.ttl");
+        assertTrue(validator.validateSchema("shexmap_example1.shex", "http://dam.example/med", "shexmap_data_example1.ttl", "http://dam.example/data/PatientX", "http://dam.example/PatientDAM"));
     }
 
 }
