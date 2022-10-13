8a9e2e6e797524c1c20586138f2affb020429953
Eric Prud'hommeaux <eric+github@w3.org>
2022-10-13 07:40:08 +0200

    `validateSchema` returns ShExReport

--- shexmap-validator/src/main/java/com/medontology/shexmap/validator/Validator.java
+++ shexmap-validator/src/main/java/com/medontology/shexmap/validator/Validator.java
@@ -14,20 +14,33 @@ public class Validator {
         System.out.println("Hello world!");
     }
 
-    public boolean validateSchema(Load data, Load schema) {
+    /**
+     * Load schema and data and validate
+     *
+     * @param schema - Load resource
+     * @param data   - Load resource
+     * @return ShExReport from validation
+     */
+    public static ShexReport validateSchema(Load schema, Load data) {
         Resolver r = new Resolver();
 
         ShexMap shapeMap = r.shapeMap(r.node(data.node, data.base), r.node(schema.node, schema.base));
-        Graph graph = r.readGraph(data.path, data.base);
+        Graph graph = r.readTurtle(data.path, data.base);
         ShexSchema schema1 = Shex.readSchema(schema.path, schema.base);
-        ShexReport report = ShexValidator.get().validate(graph, schema1, shapeMap);
-
-        return report.conforms();
+        return ShexValidator.get().validate(graph, schema1, shapeMap);
     }
+
+    /**
+     * parameter to `validateSchema` for specifying:
+     * path - file path for loadable resource
+     * base - base URI when loading that resource
+     * node - point in loaded resource (validation focus or shape name)
+     */
     public static class Load {
         String path;
         String base;
         String node;
+
         Load(String path, String base, String node) {
             this.path = path;
             this.base = base;
@@ -38,11 +51,16 @@ public class Validator {
 
 /**
  * Crap work to make Validator more literate.
- * Holds a Model and an IRIxResolver.
  */
 class Resolver {
     Model model = ModelFactory.createDefaultModel();
 
+    /**
+     * Emulate javascript's URL(rel, base) interface 'cause I [ericP] assume it has wisdom.
+     * @param rel - a relative IRI to be resolved against base, or an absolute IRI
+     * @param base - if this is null, rel should be absolute
+     * @return Node (technically, a URI_Resource, but that would need casting to Node for some reason)
+     */
     Node node (String rel, String base) {
         String resolved;
         if (base == null) {
@@ -54,14 +72,27 @@ class Resolver {
         return model.createResource(resolved).asNode();
     }
 
+    /**
+     * Construct a ShapeMap from a pair of node/shape
+     * @param node node in node/shape pair
+     * @param shapeRef shape in node/shape pair
+     * @return ShapeMap (currently called `ShExMap`, but I intend to PR that)
+     */
     public ShexMap shapeMap(Node node, Node shapeRef) {
         ShexMapBuilder mb = new ShexMapBuilder();
         mb.add(node, shapeRef);
         return mb.build();
     }
 
-    public Graph readGraph(String dataPath, String dataBase) {
-        RDFDataMgr.read(model, dataPath, dataBase, Lang.TTL);
+    /**
+     * Emulate jena-shex's readSchema interface for Turtle files
+     *
+     * @param path - path to Turtle file to parse
+     * @param base - base for URL resolution
+     * @return ShEx schema
+     */
+    public Graph readTurtle(String path, String base) {
+        RDFDataMgr.read(model, path, base, Lang.TTL);
         return model.getGraph();
     }
 }
--- shexmap-validator/src/test/java/com/medontology/shexmap/validator/ValidatorTest.java
+++ shexmap-validator/src/test/java/com/medontology/shexmap/validator/ValidatorTest.java
@@ -6,21 +6,26 @@ import static org.junit.Assert.*;
 public class ValidatorTest {
 
     @Test
-    public void test() {
-        Validator validator = new Validator();
-        assertTrue(validator.validateSchema(
-                new Validator.Load("Is1_Ip1_Io1.ttl", "http://a.example/", "s1"),
-                new Validator.Load("1iri.shex", "http://a.example/", "S1")
-        ));
+    public void shexTest_1iri_pass() {
+        assertTrue(Validator.validateSchema(
+                new Validator.Load("1iri.shex", "http://a.example/", "S1"),
+                new Validator.Load("Is1_Ip1_Io1.ttl", "http://a.example/", "s1")
+        ).conforms());
     }
 
     @Test
-    public void test2() {
-        Validator validator = new Validator();
-        assertTrue(validator.validateSchema(
-                new Validator.Load("shexmap_data_example1.ttl", "http://dam.example/data/", "PatientX"),
-                new Validator.Load("shexmap_example1.shex", "http://dam.example/", "PatientDAM")
-        ));
+    public void shexTest_1iri_fail() {
+        assertFalse(Validator.validateSchema(
+                new Validator.Load("1iri.shex", "http://a.example/", "S1"),
+                new Validator.Load("Is1_Ip1_Io1.ttl", "http://a.example/", "s999")
+        ).conforms());
     }
 
+    @Test
+    public void bpDAMexample() {
+        assertTrue(Validator.validateSchema(
+                new Validator.Load("shexmap_example1.shex", "http://dam.example/", "PatientDAM"),
+                new Validator.Load("shexmap_data_example1.ttl", "http://dam.example/data/", "PatientX")
+        ).conforms());
+    }
 }
